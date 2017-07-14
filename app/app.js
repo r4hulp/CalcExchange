7447b393534b503d2ea9ec8a81e8330cfff174d5
Rahul Patil <rahulpp@live.com>
2017-07-14 12:14:42 +0530

    remove unwanted conflict comments

--- app/app.js
+++ app/app.js
@@ -54,10 +54,7 @@ CalcExchange.App = function () {
 
     self.Uri = ko.observable();
     self.AnswerUri = "";
-<<<<<<< HEAD
     self.QuestionsUri = "";
-=======
->>>>>>> 8f3640b666c71283235c08ac5f97bee1c60a02bd
 
     self.UserProfile.subscribe(function (user) {
         console.log(user);
@@ -81,15 +78,12 @@ CalcExchange.App = function () {
         return answerUrl;
     }
 
-<<<<<<< HEAD
     self.QuestionsHistoryUri = function () {
         //2.2/users/sdfsdf/questions?fromdate=1499817600&todate=1501113600&order=desc&sort=activity&site=stackoverflow
         var questionsuri = "http://api.stackexchange.com/2.2/users/" + self.Username() + "/questions?pagesize=100&fromdate=" + Math.floor(new Date(self.FromDate()).getTime() / 1000) + "&todate=" + Math.floor(new Date(self.ToDate()).getTime() / 1000) + "&site=" + self.SiteName();
         return questionsuri;
     }
 
-=======
->>>>>>> 8f3640b666c71283235c08ac5f97bee1c60a02bd
     self.FromDate.subscribe(function (item) {
         console.log(item);
     });
@@ -107,10 +101,8 @@ CalcExchange.App = function () {
         var newDate = self.FromDate();
         var toDate = self.ToDate();
         self.AnswerUri = self.AnswerHistoryUri();
-<<<<<<< HEAD
         self.QuestionsUri = self.QuestionsHistoryUri();
-=======
->>>>>>> 8f3640b666c71283235c08ac5f97bee1c60a02bd
+
         self.Uri(self.GenerateUri());
     });
 
